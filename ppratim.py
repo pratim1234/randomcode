a387d43917663925a8665e7eb244e03e7681c7f9
pratim chakraborty <125542467+pratim1234@users.noreply.github.com>
2023-08-15 17:45:16 +0530

    Add files via upload

---
+++ ppratim.py
@@ -0,0 +1,19 @@
+z = int(input("enter the no of students you want to collect data of: "))
+  
+for z in range(1,z+1):
+     
+
+
+       a = (input("enter the name: "))
+       b = int(input("roll no: "))
+       c = int(input("class: "))
+       d = int(input("marks: "))
+       e = int(input("fees: "))
+
+       x = [a,b,c,d,e]
+
+
+       print(x);
+
+else:
+      print("out of capacity")
